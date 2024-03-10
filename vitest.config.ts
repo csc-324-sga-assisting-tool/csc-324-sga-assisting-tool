f9ed12ea25b4d2e60ea6491840a2be2f5fa3a7f8
Simon Taye <mulat.simon@gmail.com>
2024-03-10 15:22:33 -0500

    set up github actions and code coverage rules

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
- 
+import {defineConfig} from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: 'json',
+    },
   },
-})
+});
