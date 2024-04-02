c88bcae924dea9d8bc1248eb63f8e71db86cf35b
Oluwatobi Alabi <tobialabi609@gmail.com>
2024-04-02 13:37:48 -0500

    help page

---
+++ src/app/help/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+// import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { ComponentNav } from "app/page";
+import { Card, Accordion } from "flowbite-react";
+import Image from "next/image";
+
+function ComponentCard() {
+  return (
+    <Card
+      className="items-center justify-between text-center mx-auto md:w-auto h-60 p-5"
+      renderImage={() => <Image width={130} height={115} src="/mascot.png" alt="image 1" className="item-left"/>}
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Frequently Asked Questions
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+      Find links to SGA resources, descriptions of GBudgets procedure, and a simple tutorial to submit your first budget!!
+      </p>
+    </Card>
+  );
+}
+
+function ComponentAcc() {
+  return (
+    <Accordion className="bg-pallete-2 items-center justify-between mx-auto md:w-auto h-screen">
+      <Accordion.Panel>
+        <Accordion.Title className="bg-gray-100 flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 dark:text-gray-400 gap-3">
+          How do I know when my budget is approved?
+        </Accordion.Title>
+        <Accordion.Content className="bg-white">
+          <p className="text-gray-500 dark:text-gray-400">
+            Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
+            dropdowns, modals, navbars, and more.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            Check out this guide to learn how to&nbsp;
+            and start developing websites even faster with components on top of Tailwind CSS.
+          </p>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel>
+        <Accordion.Title className="flex bg-gray-100 items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 dark:text-gray-400 gap-3">
+          How do I download/reuse my past budget?
+        </Accordion.Title>
+        <Accordion.Content className="bg-white">
+          <p className="text-gray-500 dark:text-gray-400 ">
+            Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
+            has a design equivalent in our Figma file.
+          </p>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel>
+        <Accordion.Title className="flex bg-gray-100 items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 dark:text-gray-400 gap-3">
+          Tutorials: Make and submit your first budget!!!
+        </Accordion.Title>
+        <Accordion.Content className="bg-white">
+          <p className="text-gray-500 dark:text-gray-400">
+            The main difference is that the core components from Flowbite are open source under the MIT license, whereas
+            Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
+            components, whereas Tailwind UI offers sections of pages.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
+            technical reason stopping you from using the best of two worlds.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+          </ul>
+        </Accordion.Content>
+      </Accordion.Panel>
+    </Accordion>
+  );
+}
+
+
+
+export default function Home() {
+  return (
+    <main>
+      <ComponentNav/>
+      <ComponentCard/>
+      <ComponentAcc/>
+
+
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,199 @@
+"use client";
 import Image from 'next/image';
+import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+
+
+
+// import { Button, Navbar } from "flowbite-react";
+// className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:w-auto p-4"
+
+export function ComponentNav() {
+  return (
+    <Navbar fluid className='bg-pallete-1 items-center justify-between mx-auto md:w-auto p-3'>
+      <Navbar.Brand href="http://localhost:3000/csc-324-sga-assisting-tool">
+        <span className="self-center whitespace-nowrap text-xl font-semibold text-black">GBudget</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <a href="/csc-324-sga-assisting-tool/help"  className="flex items-center space-x-3 rtl:space-x-reverse"> 
+          <Button type="button" className="text-left text-black bg-pallete-1 font-medium text-sm order-1">Help</Button>
+        </a>
+        <Button className='bg-pallete-5 font-medium text-sm w-28 h-10'>Log Out</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse className="justify-between hidden w-full md:flex md:w-auto md:order-1">
+        <Navbar.Link href="#" active className='text-black text-base font-medium'>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#" className='text-black text-base font-medium'>About</Navbar.Link>
+        <Navbar.Link href="#" className='text-black text-base font-medium'>Services</Navbar.Link>
+        <Navbar.Link href="#" className='text-black text-base font-medium'>Pricing</Navbar.Link>
+        <Navbar.Link href="#" className='text-black text-base font-medium'>Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main >
+      {/* <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1> */}
+      <ComponentNav/>
+      
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50 text-balance'}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  );
+  )
 }
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+//         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+//           Get started by editing&nbsp;
+//           <code className="font-mono font-bold">src/app/page.tsx</code>
+//         </p>
+//         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+//           <a
+//             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             By{' '}
+//             <Image
+//               src="/vercel.svg"
+//               alt="Vercel Logo"
+//               className="dark:invert"
+//               width={100}
+//               height={24}
+//               priority
+//             />
+//           </a>
+//         </div>
+//       </div>
+
+//       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+//         <Image
+//           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+//           src="/next.svg"
+//           alt="Next.js Logo"
+//           width={180}
+//           height={37}
+//           priority
+//         />
+//       </div>
+
+//       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+//         <a
+//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={'mb-3 text-2xl font-semibold'}>
+//             Docs{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
+//             Find in-depth information about Next.js features and API.
+//           </p>
+//         </a>
+
+//         <a
+//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={'mb-3 text-2xl font-semibold'}>
+//             Learn{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
+//             Learn about Next.js in an interactive course with&nbsp;quizzes!
+//           </p>
+//         </a>
+
+//         <a
+//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={'mb-3 text-2xl font-semibold'}>
+//             Templates{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
+//             Explore starter templates for Next.js.
+//           </p>
+//         </a>
+
+//         <a
+//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2 className={'mb-3 text-2xl font-semibold'}>
+//             Deploy{' '}
+//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+//               -&gt;
+//             </span>
+//           </h2>
+//           <p className={'m-0 max-w-[30ch] text-sm opacity-50 text-balance'}>
+//             Instantly deploy your Next.js site to a shareable URL with Vercel.
+//           </p>
+//         </a>
+//       </div>
+//     </main>
+//   );
+// }
+
+
+
+
+// <nav className="bg-pallete-1 border-gray-200 dark:bg-gray-900">
+//         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:w-auto p-4">
+//           <a href="http://localhost:3000/csc-324-sga-assisting-tool" className="flex items-center space-x-3 rtl:space-x-reverse">
+//               <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+//               <span className="text-black self-center text-2xl font-semibold whitespace-nowrap dark:text-black">GBudget</span>
+//           </a>
+//           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+//             <a href="/help"  className="flex items-center space-x-3 rtl:space-x-reverse"> 
+//               <button type="button" className="text-left text-black bg-pallete-1 hover:bg-blue-800  font-medium text-sm order-1">Help</button>
+//             </a>
+//             <button type="button" className="text-white bg-pallete-5 order-2 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log Out</button>
+//             {/* <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+//               <span className="sr-only">Open main menu</span>
+//               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+//                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+//               </svg>
+//             </button> */}
+//           </div>
+//           <div className="justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+//             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-pallete-1 ">
+//               <li>
+//                 <a href="#" className="block py-2 px-3 md:p-0 text-black bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
+//               </li>
+//               <li>
+//                 <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+//               </li>
+//               <li>
+//                 <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+//               </li>
+//               <li>
+//                 <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
