e79610133a178ce8086ecf436ecfb123053017a8
Diogo <tandetat@grinnell.edu>
2024-04-04 16:24:29 -0500

    Start CI Firestore Emulator setup + testing for backend

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    environment: 'jsdom',
+    environment: 'node',
     coverage: {
       provider: 'v8',
       reporter: 'json',
