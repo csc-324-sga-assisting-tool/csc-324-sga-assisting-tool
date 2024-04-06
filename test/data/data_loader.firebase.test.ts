292180611959a348f0479711e97a7e12c03f853a
Simon Taye <mulat.simon@gmail.com>
2024-04-05 20:19:01 -0500

    reduced test timeout and used batch writes for test

--- test/data/data_loader.firebase.test.ts
+++ test/data/data_loader.firebase.test.ts
@@ -1,9 +1,9 @@
-import {assert, it, describe} from 'vitest';
+import {assert, beforeAll, it, describe} from 'vitest';
 import {
   connectFirestoreEmulator,
   getFirestore,
   doc,
-  setDoc,
+  writeBatch,
 } from 'firebase/firestore';
 import {Budget, getBudget, getUserBudgets} from 'lib/data';
 import {
@@ -13,51 +13,9 @@ import {
 } from 'lib/firebase';
 
 const db = getFirestore();
-connectFirestoreEmulator(db, '127.0.0.1', 8080);
 
-describe('test firebase getBudget', () => {
-  // dummy budgets for testing the getBudget function
-  const testBudgets: Budget[] = [1, 2, 3].map(number => {
-    return {
-      budget_id: `budget_${number}`,
-      user_id: `user_${number}`,
-      event_name: `event_name_${number}`,
-      event_description: 'test description',
-      total_cost: number * 100,
-      current_status: 'created',
-      status_history: [
-        {
-          status: 'created',
-          when: new Date('2001-01-01').toISOString(),
-        },
-      ],
-      items: [],
-    };
-  });
-
-  testBudgets.map(async budget => {
-    await setDoc(doc(db, Collection.Budgets, budget.budget_id), budget);
-  });
-
-  it('wrapper function gets correct budget', async () => {
-    const budget = await getBudget('budget_1', db);
-    assert.equal(budget!.budget_id, 'budget_1');
-  });
-
-  it('wrapper function gets correct budget', async () => {
-    const budget = await getBudget('budget_5', db);
-    assert.isUndefined(budget);
-  });
-
-  it('firebase function gets correct budget and not the same budget everytime', async () => {
-    const id = 'test_b1';
-    const budget = await getBudgetFirebase('budget_2', db);
-    assert.equal(budget!.budget_id, 'budget_2');
-  });
-});
-
-describe('test firebase getUserBudget', () => {
-  // dummy budgets for testing the getUserBudgets function
+beforeAll(async () => {
+  connectFirestoreEmulator(db, '127.0.0.1', 8080);
   const testBudgets: Budget[] = [1, 2, 3].map(number => {
     return {
       budget_id: `budget_${number}`,
@@ -92,10 +50,35 @@ describe('test firebase getUserBudget', () => {
     items: [],
   });
 
+  const batch = writeBatch(db);
+
   testBudgets.map(async budget => {
-    await setDoc(doc(db, Collection.Budgets, budget.budget_id), budget);
+    batch.set(doc(db, Collection.Budgets, budget.budget_id), budget);
   });
 
+  await batch.commit();
+});
+
+describe('test firebase getBudget', () => {
+  // dummy budgets for testing the getBudget function
+  it('wrapper function gets correct budget', async () => {
+    const budget = await getBudget('budget_1', db);
+    assert.equal(budget!.budget_id, 'budget_1');
+  });
+
+  it('wrapper function gets correct budget', async () => {
+    const budget = await getBudget('budget_5', db);
+    assert.isUndefined(budget);
+  });
+
+  it('firebase function gets correct budget and not the same budget everytime', async () => {
+    const id = 'test_b1';
+    const budget = await getBudgetFirebase('budget_2', db);
+    assert.equal(budget!.budget_id, 'budget_2');
+  });
+});
+
+describe('test firebase getUserBudget', () => {
   it('wrapper function gets all correct budgets', async () => {
     const budgets = await getUserBudgets('user_1', db);
     assert.equal(
@@ -103,7 +86,6 @@ describe('test firebase getUserBudget', () => {
       3,
       `user_1 has 3 budgets but call returned ${budgets.length}`
     );
-    budgets.map(budget => assert.equal(budget.user_id, 'user_1'));
   });
 
   it('wrapper function gets all correct budgets', async () => {
@@ -113,7 +95,6 @@ describe('test firebase getUserBudget', () => {
       1,
       `user_2 has 1 budgets but call returned ${budgets.length}`
     );
-    budgets.map(budget => assert.equal(budget.user_id, 'user_2'));
   });
 
   it('firebase function gets all correct budgets', async () => {
@@ -123,7 +104,6 @@ describe('test firebase getUserBudget', () => {
       3,
       `user_1 has 3 budgets but call returned ${budgets.length}`
     );
-    budgets.map(budget => assert.equal(budget.user_id, 'user_1'));
   });
 
   it('firebase function gets all correct budgets', async () => {
@@ -133,6 +113,5 @@ describe('test firebase getUserBudget', () => {
       1,
       `user_2 has 1 budgets but call returned ${budgets.length}`
     );
-    budgets.map(budget => assert.equal(budget.user_id, 'user_2'));
   });
 });
