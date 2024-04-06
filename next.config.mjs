4a032833c0a2b7d5107438580aa2d974c83c69d6
Simon Taye <mulat.simon@gmail.com>
2024-04-05 21:46:15 -0500

    remove prefixing required by GH pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/csc-324-sga-assisting-tool',
-  assetPrefix: '/csc-324-sga-assisting-tool/',
 };
 
 export default nextConfig;
