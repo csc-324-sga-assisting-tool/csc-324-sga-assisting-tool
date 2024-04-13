4f1e68b6fe04bf5916fcdde2c8f980fc80944b4f
Elliot-TS <elliotbb@believershome.com>
2024-04-13 11:08:59 -0500

    Removed old data_loader.firebase export from src/lib/data/index.ts

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,9 +1,2 @@
-export {
-  getBudget,
-  getUserBudgets,
-  addBudget,
-  FirebaseModifier,
-  FirebaseProvider,
-} from './data_loader.firebase';
 export type {Budget, Status, Item} from './data_types';
 export {DataModifier, DataProvider} from './data_loader';
