1087eb28085db34d4196d488a7e87cdf7ef7b805
Simon Taye <mulat.simon@gmail.com>
2024-04-14 16:00:59 -0500

    test that budget displays

--- test/app/dashboard.test.tsx
+++ test/app/dashboard.test.tsx
@@ -45,6 +45,43 @@ describe('Test Dashboard works as Expected', () => {
     expect(screen.queryByTestId('Cost')).toBeNull();
   });
 
+  it('displays budget and sidebar correctly', async () => {
+    // make sure there are no budgets
+    mockDataprovider.setBudgets([{
+      user_id: 'test_user',
+      budget_id: 'test_budget',
+      event_name: "Test Event",
+      event_description: "Test Event Description",
+      current_status: 'submitted',
+      total_cost: 123,
+      status_history: [{ status: 'submitted', when: new Date().toISOString() }],
+      items: [],
+    }]);
+
+    //render the daashboard
+    const props = {
+      userID: 'test_user',
+      dataProvider: mockDataprovider,
+      dataModifier: mockDataprovider,
+      TESTING_FLAG: true,
+    };
+    // await Dashboard since it is an async component
+    render(await Dashboard({ ...props }));
+
+    // Check that the side bar is rendered
+    expect(screen.queryByText('Summary')).toBeInTheDocument();
+    // Check that the remaining budget is rendered
+    expect(screen.queryByText('$ 100')).toBeInTheDocument();
+    // Check that the Add Button is rendered
+    expect(
+      screen.queryByTestId('new-budget-form-button-add')
+    ).toBeInTheDocument();
+    // 0 dollars, the cost of the budget should be rendered
+    expect(await screen.findByText('123', { exact: false })).toBeInTheDocument();
+    // Test Event, the name of the budget should be rendered
+    expect(await screen.findByText('Test Event')).toBeInTheDocument();
+  });
+
   it('make budget forms creates a new budget', async () => {
     // setup the userEvents library
     const user = userEvent.setup();
