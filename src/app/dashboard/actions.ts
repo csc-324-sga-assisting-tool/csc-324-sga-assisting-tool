2d416026cce7d745063ca5e1c162a37379c05776
Simon Taye <mulat.simon@gmail.com>
2024-04-14 15:51:27 -0500

    refactor to reduce duplication

--- src/app/dashboard/actions.ts
+++ src/app/dashboard/actions.ts
@@ -2,14 +2,13 @@
 import {revalidatePath} from 'next/cache';
 import {Budget, DataModifier, FirebaseModifier} from 'lib/data';
 
-export async function TESTcreateBudgetAction(
-  dataModifier: DataModifier = FirebaseModifier,
+function createBudget(
   user_id: string,
   event_name: string,
   event_description: string
-): Promise<void> {
+): Budget {
   const budget_id = `${user_id}-${event_name}-${new Date().getSeconds()}`;
-  const budget: Budget = {
+  return {
     budget_id,
     user_id,
     event_name,
@@ -24,8 +23,17 @@ export async function TESTcreateBudgetAction(
       },
     ],
   };
+}
 
-  return dataModifier.addBudget(budget);
+export async function TESTcreateBudgetAction(
+  dataModifier: DataModifier = FirebaseModifier,
+  user_id: string,
+  event_name: string,
+  event_description: string
+): Promise<void> {
+  return dataModifier.addBudget(
+    createBudget(user_id, event_name, event_description)
+  );
 }
 
 export async function createBudgetAction(
@@ -33,23 +41,7 @@ export async function createBudgetAction(
   event_name: string,
   event_description: string
 ): Promise<void> {
-  const budget_id = `${user_id}-${event_name}-${new Date().getSeconds()}`;
-  const budget: Budget = {
-    budget_id,
-    user_id,
-    event_name,
-    event_description,
-    total_cost: 0,
-    items: [],
-    current_status: 'created',
-    status_history: [
-      {
-        status: 'created',
-        when: new Date().toISOString(),
-      },
-    ],
-  };
-
+  const budget = createBudget(user_id, event_name, event_description);
   const result = FirebaseModifier.addBudget(budget);
   revalidatePath('/dashboard');
   return result;
