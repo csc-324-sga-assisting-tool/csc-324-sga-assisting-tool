ead9b1c96558dd52b982ab3dd5982a061819aeef
Oluwatobi Alabi <tobialabi609@gmail.com>
2024-04-23 16:40:04 -0500

    fixed revalidate

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,10 @@
 import {verifySession} from 'app/dal';
 import {Dashboard} from './dashboard';
 import {DataModel, Database} from 'lib/data';
+import {revalidatePath} from 'next/cache';
 
 export default async function Page() {
+  revalidatePath('/dashboard');
   const session = await verifySession();
   const userId = session.userId;
   console.log(`User id from session is: ${userId}`);
