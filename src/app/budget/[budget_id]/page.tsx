ba9fa526be38f4cb7265b83676ec71162339d7b3
Elliot-TS <elliotbb@believershome.com>
2024-05-07 09:02:22 -0500

    Fixed authentication for budget page

--- src/app/budget/[budget_id]/page.tsx
+++ src/app/budget/[budget_id]/page.tsx
@@ -2,24 +2,30 @@
 import {RSOBudgetView} from './budgetview';
 import {SGABudgetView} from './sgaBudgetView';
 import {DataModel, Database} from 'lib/data';
-import {verifySession} from 'app/dal';
 import {userIsSGA} from 'lib/data/utils';
+import {AuthModel} from 'lib/data/auth_model';
+import {redirect} from 'next/navigation';
 
 export default async function Page({params}: {params: {budget_id: string}}) {
   const db = Database;
   const dataModel = new DataModel(db);
+  const auth = AuthModel;
 
-  const session = await verifySession();
-  const userId = session.userId;
-  const user = await dataModel.getUser(userId);
-  const isSGA = userIsSGA(user);
-  return isSGA ? (
-    <SGABudgetView
-      budget_id={params.budget_id}
-      user_id={userId}
-      dataModel={dataModel}
-    />
-  ) : (
-    <RSOBudgetView budget_id={params.budget_id} dataModel={dataModel} />
-  );
+  try {
+    const userId = await auth.getSignedInUser();
+    const user = await dataModel.getUser(userId);
+    const isSGA = userIsSGA(user);
+    return isSGA ? (
+      <SGABudgetView
+        budget_id={params.budget_id}
+        user_id={userId}
+        dataModel={dataModel}
+      />
+    ) : (
+      <RSOBudgetView budget_id={params.budget_id} dataModel={dataModel} />
+    );
+  } catch (error) {
+    console.log(error!.toString());
+    redirect('/');
+  }
 }
